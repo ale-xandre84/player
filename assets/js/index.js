0f37a6f20183a025649a1866b1d633d4103ffd9c
ale-xandre84 <c.alexandresouza84@gmail.com>
2024-11-19 11:44:57 -0300

    finalizando o estilo

---
+++ assets/js/index.js
@@ -0,0 +1,28 @@
+
+let musicData = [
+      {
+            name: "Represent",
+            singer: "Orishas",
+            src: "music-Orishas.mp3",
+
+      },
+
+       {
+            name: "Chunk at the Deuce",
+            singer: "lill Keke",
+            src: "music-Chunk at the Deuce.mp3",
+
+       },
+
+
+    {
+       name: "Grillz",
+       singer: "Nelly",
+       src: "music-Grillz.mp3",
+
+    } 
+
+]
+
+
+const container=document.querySelector('container')
